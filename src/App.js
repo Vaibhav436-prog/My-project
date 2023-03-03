e23c08188c3246596ac3f1117b86b1420da8b333
Ayushack101 <ayushtyagi772@gmail.com>
2023-03-03 14:00:55 +0530

    Ayush Industries

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import './App.css';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import Table from './components/Table';
-import SignUp from './components/SignUp';
-import SignIn from './components/SignIn';
-import ForgotPassword from './components/ForgotPassword';
+import "./App.css";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Table from "./components/Table";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import ForgotPassword from "./components/ForgotPassword";
 
 function App() {
   return (
-   <>
-   <h1 className='vaibhav'>Vaibhav Industries</h1>
-   <BrowserRouter>
-   <Routes>
-   <Route path="/" element={<SignUp />}></Route>
-   <Route path="/profile" element={<Table/>}></Route>
-   <Route path="/login" element={<SignIn/>}></Route>
-   <Route path="/forgot_password" element={<ForgotPassword/>}></Route>
-
-   </Routes>
-   </BrowserRouter>
-   </>
+    <>
+      <h1 className="vaibhav">Ayush Industries</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignUp />}></Route>
+          <Route path="/profile" element={<Table />}></Route>
+          <Route path="/login" element={<SignIn />}></Route>
+          <Route path="/forgot_password" element={<ForgotPassword />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
