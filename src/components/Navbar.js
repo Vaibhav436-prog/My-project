92cf0b9951efd6909d0db5046135c51c3e610b58
Ayushack101 <ayushtyagi772@gmail.com>
2023-03-07 17:26:55 +0530

    New navbar and banner added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,102 +1,71 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useRef } from "react";
 
 function Navbar() {
-  const navigate = useNavigate();
-  const logOut = () => {
-    navigate("/");
-  };
+  const right = useRef(null);
+  const subMenu = useRef(null);
+  function handleNavToggle() {
+    right.current.classList.toggle("show-right");
+  }
+  function handleSubBtn() {
+    subMenu.current.classList.toggle("show-dropdown");
+  }
+
+  const navbar = useRef(null);
+  useEffect(() => {
+    function handleStickyNavbar() {
+      navbar.current.classList.toggle("sticky", window.scrollY > 25);
+    }
+    window.addEventListener("scroll", handleStickyNavbar);
+    return () => {
+      window.removeEventListener("scroll", handleStickyNavbar);
+    };
+  }, []);
   return (
     <>
       {/* <!-- Header --> */}
-      <div className="sub-header">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 col-xs-12">
-              <ul className="left-info">
-                <li>
-                  <a href="#">
-                    <i className="fa fa-clock-o"></i>Mon-Fri 09:00-17:00
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <i className="fa fa-phone"></i>090-080-0760
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-md-4">
-              <ul className="right-icons">
-                <li>
-                  <a href="#">
-                    <i className="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <i className="fa fa-twitter"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <i className="fa fa-linkedin"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <i className="fa fa-behance"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <header className="">
-        <nav className="navbar navbar-expand-lg">
-          <div className="container">
-            <a className="navbar-brand" href="index.html">
+      <header>
+        {/* <!-- Navigation bar --> */}
+        <nav className="navbar" ref={navbar}>
+          <div className="navbar-center">
+            <div className="logo">
               <h2>Ocean Vista</h2>
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarResponsive"
-              aria-controls="navbarResponsive"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item active">
-                  <a className="nav-link" href="#top">
-                    Home
-                    <span className="sr-only">(current)</span>
-                  </a>
+              <button className="nav-toggle" onClick={handleNavToggle}>
+                <i className="fas fa-bars"></i>
+              </button>
+            </div>
+            <div className="right" ref={right}>
+              <ul className="menu">
+                <li className="menu-items">
+                  <a href="#home">Home</a>
                 </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    About Us
-                  </a>
+                <li className="menu-items">
+                  <a href="#about">About</a>
                 </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Our Services
+                <li className="menu-items">
+                  <a href="#" className="sub-btn" onClick={handleSubBtn}>
+                    Services<i className="fa-solid fa-caret-down side"></i>
                   </a>
+                  <ul className="sub-menu" ref={subMenu}>
+                    <li className="sub-items">
+                      <a href="#">sub-items1</a>
+                    </li>
+                    <li className="sub-items">
+                      <a href="#">sub-items2</a>
+                    </li>
+                    <li className="sub-items">
+                      <a href="#">sub-items3</a>
+                    </li>
+                    <li className="sub-items">
+                      <a href="#">sub-items4</a>
+                    </li>
+                  </ul>
                 </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Contact Us
-                  </a>
+                <li className="menu-items">
+                  <a href="#contact">Contact</a>
                 </li>
               </ul>
-              <button type="button" class="btn btn-primary">
-                Log in
+              <button className="btn">
+                <a href="#">Log in</a>
               </button>
             </div>
           </div>
